d491ec36b9a13a621ff7302ab71777b2e9b4ff91
cafeluta <sebi.ionita23@gmail.com>
2025-03-17 18:48:18 +0200

    Solved binary_search
    Signed-off-by: cafeluta <sebi.ionita23@gmail.com>

--- labs/lab-03/tasks/binary_search/support/binary_search.c
+++ labs/lab-03/tasks/binary_search/support/binary_search.c
@@ -10,8 +10,24 @@ int binary_search(int *v, int len, int dest)
 	int end = len - 1;
 	int middle;
 
-	/**
-	 * TODO: Implement binary search
-	 */
+loop:
+	if (start > end)
+		return -1;
+	middle = (start + end) / 2;
+	if (v[middle] == dest)
+		goto res;
+
+	if (v[middle] > dest)
+		goto smaller;
+	
+	start = middle + 1;
+	goto loop;
+
+smaller:
+	end = middle - 1;
+	goto loop;
+
+res:
+	return middle;
 	return -1;
 }
